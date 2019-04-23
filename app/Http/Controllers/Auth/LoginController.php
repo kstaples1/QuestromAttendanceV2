b4db6536405bdc99e9a2214cdeb9536abc18c283
Kstaples1 <kstapes@bu.edu>
2019-04-23 17:41:46 -0400

    completed

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+use App\global_user_group;
 
 class LoginController extends Controller
 {
@@ -25,7 +27,34 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    //protected $redirectTo = '/home';
+    public function redirectTo()
+    {
+        $id_user = Auth::id();
+        $userGroup = global_user_group::where('id_user','=',$id_user)
+            ->join('user_groups','global_user_group.id_userGroup','=','user_groups.id_userGroup')
+            ->orderby('priorityLevel')
+            ->first();
+
+        $role = $userGroup->groupName;
+
+        // Check user role
+        switch ($role) {
+            case 'Admin':
+                return '/admin';
+                break;
+            case 'Professor':
+                return '/professor';
+                break;
+            case 'Student':
+                return '/student';
+                break;
+            default:
+                return '/login';
+                break;
+        }
+
+    }
 
     /**
      * Create a new controller instance.
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -6,4 +6,11 @@
  * Time: 1:14 PM
  */
 
+$id_user = Auth::id();
+$userGroup = \App\global_user_group::where('id_user','=',$id_user)
+    ->join('user_groups','global_user_group.id_userGroup','=','user_groups.id_userGroup')
+    ->orderby('priorityLevel')
+    ->first();
+echo $userGroup->groupName;
+
 ?>
