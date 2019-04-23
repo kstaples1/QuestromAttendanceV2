88950a3c46eae6f7277ebf6c85baeb1d4d39bf7f
Kstaples1 <kstapes@bu.edu>
2019-04-23 13:06:56 -0400

    starting project

---
+++ app/User.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'BUID','firstName','lastName','userName', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+
+    /** This is where we make connections to the different tables **/
+
+    protected $primaryKey = 'id_user';
+
+    public function user_groups()
+    {
+        return $this->belongsToMany('App\user_groups', 'global_user_group','id_user','id_userGroup');
+    }
+
+    public function master_courses()
+    {
+        return $this->belongsToMany('App\master_courses', 'semester_courses','id_master','id_master');
+    }
+
+    public function semester_courses()
+    {
+        return $this->hasMany('App\semester_courses','id_user','id_creator');
+    }
+
+    public function enrollment()
+    {
+        return $this->hasMany('App\enrollment','id_user', 'id_user');
+    }
+
+    public function student_answers()
+    {
+        return $this->hasOne('App\student_answers','id_user','id_user');
+    }
+
+
+}
---
+++ readme.md
@@ -0,0 +1,20 @@
+<p align="center"><img src="https://laravel.com/assets/img/components/logo-laravel.svg"></p>
+
+<p align="center">
+<a href="https://travis-ci.org/laravel/framework"><img src="https://travis-ci.org/laravel/framework.svg" alt="Build Status"></a>
+<a href="https://packagist.org/packages/laravel/framework"><img src="https://poser.pugx.org/laravel/framework/d/total.svg" alt="Total Downloads"></a>
+<a href="https://packagist.org/packages/laravel/framework"><img src="https://poser.pugx.org/laravel/framework/v/stable.svg" alt="Latest Stable Version"></a>
+<a href="https://packagist.org/packages/laravel/framework"><img src="https://poser.pugx.org/laravel/framework/license.svg" alt="License"></a>
+</p>
+
+## About Questrom Attendance
+
+The Questrom Attendance app is used so that admins can create classes and professors can make sections for each class and students can take classes and take quizes created by professors.
+
+## Project Layout
+
+The project was make using Laravel which is a framework of PHP and uses a MVC or model view controller framework.
+
+### Controllers
+   Controllers are found under App/Http/Controllers and they facilitate the transfer of data from the database (model) and the front end (view).
+   
