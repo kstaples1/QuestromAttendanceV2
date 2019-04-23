909c5327a8f1d1435685a5bf5124c4b34eb1ce08
Kstaples1 <kstapes@bu.edu>
2019-04-23 18:06:35 -0400

    updated

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/test','HomeController@test')->name('home');
+
+Route::get('/test','HomeController@test')->middleware('admin');
 
 
 /********************** This is the admin routes **********************/
